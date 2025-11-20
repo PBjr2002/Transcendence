1f7603053600b7f12ed109c454cdb34e56506f9f
Paulo <paulobernardo.2002@gmail.com>
2025-11-20 16:19:35 +0000

    changed the database docker from alpine to debian and also reseted the profile pictures of each user

--- database/database_init.sh
+++ database/database_init.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-set -euo pipefail
+set -eu pipefail
 
 DB_PATH="/SQLite_data/database.db"
 TABLES="./tables.sql"
 
 mkdir -p "$(dirname "$DB_PATH")"
-
 
 if [ ! -f "$DB_PATH" ]; then
   sqlite3 "$DB_PATH" < "$TABLES"
