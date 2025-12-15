957c1db16834cf5374f93d9a2e488a9a740de10f
Andre Santos <deblin.pt@gmail.com>
2025-12-15 18:46:35 +0000

    Many changes in Kibana, Logstash and elasticsearch. problem with auth between logstash and elasticsearch

---
+++ elasticsearch/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+until curl -k -u elastic:${ELASTIC_PASSWORD} http://elasticsearch:9200; do
+  echo "Waiting for Elasticsearch..."
+  sleep 3
+done
+
+echo "Creating logstash_writer role..."
+curl -u elastic:${ELASTIC_PASSWORD} -X POST "http://localhost:9200/_security/user/logstash_system/_password" -H "Content-Type: application/json" -d '{"password": "${LOGSTASH_PASSWORD}"}'
+
+echo "Creating logstash user..."
+curl -u elastic:${ELASTIC_PASSWORD} -X POST "http://localhost:9200/_security/user/kibana_system/_password" -H "Content-Type: application/json" -d '{"password": "${ELASTICSEARCH_PASSWORD}"}'
+
+echo "Security bootstrap completed."
