b0667bed277d15ca8c7cea948be5371f9b4ccd22
Andre Santos <deblin.pt@gmail.com>
2026-01-06 18:26:08 +0000

    it works!!! now I have to get rid of the passwords writen every where

--- elasticsearch/setup.sh
+++ elasticsearch/setup.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-until curl -k -u elastic:${ELASTIC_PASSWORD} http://elasticsearch:9200; do
+until curl -k -u elastic:UMApassWord123 http://elasticsearch:9200; do
   echo "Waiting for Elasticsearch..."
-  sleep 3
+  sleep 2
 done
 
+
 echo "Creating logstash_writer role..."
-curl -u elastic:${ELASTIC_PASSWORD} -X POST "http://localhost:9200/_security/user/logstash_system/_password" -H "Content-Type: application/json" -d '{"password": "OutraPASSword"}'
+curl -u elastic:UMApassWord123 -X POST "http://localhost:9200/_security/user/logstash_system/_password" -H "Content-Type: application/json" -d '{"password": "OutraPASSword"}'
 
 echo "Creating logstash user..."
-curl -u elastic:${ELASTIC_PASSWORD} -X POST "http://localhost:9200/_security/user/kibana_system/_password" -H "Content-Type: application/json" -d '{"password": "KibanaPass"}'
+curl -u elastic:UMApassWord123 -X POST "http://localhost:9200/_security/user/kibana_system/_password" -H "Content-Type: application/json" -d '{"password": "KibanaPass"}'
 
 echo "Security bootstrap completed."
